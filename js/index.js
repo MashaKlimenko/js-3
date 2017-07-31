bd9e56df810a7e25935e018b4254ed6c6e6fe06d
Mariia Klymenko <mashaklimenko42@gmail.com>
2017-07-31 11:37:31 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
-var clientData = ['John', 'James', 'Jacke'];
+var clientData = ['John', 'James', 'Jakobs'];
 
+var $clientData = document.getElementById('client-data');
 
-var $clientData = document.getElementById('client-data')[0];
 clientData.forEach(function(el){
-    $clientData.innerHTML += '<li>' + el + '</li>';
+   $clientData.innerHTML += `<li>${el}</li>`;
 });
+var h1 = document.getElementsByTagName('h1')[0];
+h1.addEventListener('click', function() {
+    alert('Goodbye!');
+});
+h1.addEventListener('click', function() {
+    alert('Hello');
+});
+/*var googleLink = document.getElementsByTagName('a')[0];
 
-
-var h1 = document.getElementByTagName('h1')[0];
-h1.style.display = 'none';
-
+googleLink.addEventListener('click', function(e){
+    e.preventDefault();
+    alert('No Google!');
+});*/
